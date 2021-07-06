a449284134ea0a0c88066c463bd95b2c2b62b3a4
Ezequiel Rangel <chekelinho@gmail.com>
2021-07-06 12:11:47 -0600

    Actualiza archivos de configuracion

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
+
+declare global {
+  type Func<R = void> = () => R;
+
+  type Func<P, R> = (arg: P) => R;
+
+  type React$Node = ReactNode;
+}
