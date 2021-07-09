747fbf02b279860c8bd60586da386c6166b005ab
Efraín Rochín <erochin@grupoadhoc.mx>
2021-07-09 11:14:49 -0600

    mejora la manera en que se exporta e importa los componentes

--- components/ui/Link/Link.tsx
+++ components/ui/Link/Link.tsx
@@ -1,7 +1,12 @@
 import styles from './Link.module.css';
+import { FC, PropsWithChildren } from 'react';
 
-const Link = ({ title, children }) => (
-  <a className={styles.linkContainer}>
+interface Props {
+  title?: string;
+}
+
+const Link: FC<PropsWithChildren<Props>> = ({ title, children }) => (
+  <a className={styles.root}>
     <h2 className={styles.linkTitle}>{title}</h2>
     {children}
   </a>
