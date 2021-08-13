59d756040f3a0fcfe071e9ae16729ca266fef3df
Ezequiel Rangel <chekelinho@gmail.com>
2021-08-13 13:29:06 -0600

    Reconfigura breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,6 +35,16 @@ module.exports = {
       DEFAULT: '24px',
       full: '9999px',
     },
+    screens: {
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {
       colors: {
         transparent: 'transparent',
