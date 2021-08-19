31badde5802bced1c6fafd53f7e68715cf0bacd7
Ezequiel Rangel <chekelinho@gmail.com>
2021-08-19 13:08:50 -0600

    Termino maqueta de prestamos personales

--- pages/prestamos-personales.tsx
+++ pages/prestamos-personales.tsx
@@ -1,9 +1,18 @@
 import Image from 'next/image';
 import { NextPage } from "next";
 
+import { Card, ButtonDropdown } from '~/components/ui';
+import { Layout, Navbar, VStack } from "~/components/layout";
 import { FullBleedBanner, Hero, ServicePaymentCards } from "~/components/common";
-import { Layout, Navbar } from "~/components/layout";
-import { Card } from '~/components/ui';
+
+const whatsappList = [
+  { label: "Culiacán y Navolato" },
+  { label: "Mazatlán" },
+  { label: "Guadalajara" },
+  { label: "Hermosillo" },
+  { label: "Mexicali" },
+  { label: "Tijuana" }
+];
 
 const PrestamosPage: NextPage = () => {
   return (
@@ -52,10 +61,10 @@ const PrestamosPage: NextPage = () => {
                 {requisitos.map(item => (
                   <Card key={item.id}>
                     <div>
-                      <div className="w-[186px] h-[186px] relative mx-auto">
+                      <div className="w-[120px] h-[120px] relative mx-auto m-10">
                         <Image
-                          width={186}
-                          height={186}
+                          width={120}
+                          height={120}
                           layout="responsive"
                           src={item.imageSrc}
                           alt={item.label}
@@ -75,15 +84,15 @@ const PrestamosPage: NextPage = () => {
         <section className="py-12 sm:py-24">
           <h2 className="text-2xl text-center">¿Cómo funciona?</h2>
           <div className="grid gap-6 mt-12 sm:gap-12 sm:grid-cols-2 lg:grid-cols-4">
-            {howItWorks.map(item => (
+            {steps.map(item => (
               <div
                 key={item.id}
                 className="text-center p-2"
               >
-                <div className="w-[162px] h-[162px] relative mx-auto mb-4">
+                <div className="w-[100px] h-[100px] relative mx-auto mb-4">
                   <Image
-                    width={162}
-                    height={162}
+                    width={100}
+                    height={100}
                     layout="responsive"
                     src={item.imageSrc}
                     alt={item.title}
@@ -101,7 +110,7 @@ const PrestamosPage: NextPage = () => {
           </div>
         </section>
         <section className="py-12 sm:py-24">
-          <h2 className="text-2xl">
+          <h2 className="text-2xl text-center">
             Beneficios de los préstamos personales Maxilana
           </h2>
           <div className="mt-12 grid gap-6 sm:gap-12 sm:grid-cols-2 lg:grid-cols-4">
@@ -110,10 +119,10 @@ const PrestamosPage: NextPage = () => {
                 key={item.id}
                 className="p-2"
               >
-                <div className="w-[80px] h-[80px] relative mb-4">
+                <div className="w-[70px] h-[70px] relative mb-4">
                   <Image
-                    width={80}
-                    height={80}
+                    width={70}
+                    height={70}
                     layout="responsive"
                     src={item.imageSrc}
                     alt={item.title}
@@ -130,6 +139,41 @@ const PrestamosPage: NextPage = () => {
             ))}
           </div>
         </section>
+        <section>
+          <FullBleedBanner backgroundColor="#79BDFF99">
+            <div className="max-w-7xl mx-auto px-4 pt-12">
+              <div className="sm:flex flex-row-reverse items-center">
+                <div className="mb-4 max-w-[700px]">
+                  <VStack>
+                    <h2 className="text-2xl mb-4">
+                      ¿Te interesa dar crédito a tus colaboradores?
+                    </h2>
+                    <p>
+                      Nosotros tenemos una opción para tí
+                    </p>
+                    <ButtonDropdown
+                      size="small"
+                      theme="primary"
+                      label="Más información"
+                      items={whatsappList}
+                    />
+                  </VStack>
+                </div>
+                <div className="mr-4 flex-grow">
+                  <Image
+                    width={519}
+                    height={628}
+                    quality={80}
+                    layout="responsive"
+                    objectFit="contain"
+                    src="/prestamos-credito-empresas.png"
+                    alt="Hombre cruzado de brazos"
+                  />
+                </div>
+              </div>
+            </div>
+          </FullBleedBanner>
+        </section>
       </Layout>
     </div>
   )
@@ -139,47 +183,47 @@ const requisitos = [
   {
     id: 1,
     label: "IFE o INE Vigente",
-    imageSrc: "/requisitos-prestamo-01.png"
+    imageSrc: "/svg/icono-ine.svg",
   },
   {
     id: 2,
     label: "Comprobante de ingresos (último mes)",
-    imageSrc: "/requisitos-prestamo-02.png"
+    imageSrc: "/svg/icono-voucher.svg",
   },
   {
     id: 3,
     label: "Comprobante de domicilio reciente (no más de 3 meses)",
-    imageSrc: "/requisitos-prestamo-03.png"
+    imageSrc: "/svg/icono-comprobante-domicilio.svg",
   },
   {
     id: 4,
     label: "2 referencias personales y familiares",
-    imageSrc: "/requisitos-prestamo-04.png"
+    imageSrc: "/svg/icono-referencias.svg",
   },
 ]
 
-const howItWorks = [
+const steps = [
   {
     id: 1,
-    imageSrc: "/pasos-prestamo-01.png",
+    imageSrc: "/svg/icono-computadora.svg",
     title: "Simula tu crédito y solicita tu préstamo",
     description: "Ingresa la información solicitada en el formularío. Empieza ya."
   },
   {
     id: 2,
-    imageSrc: "/pasos-prestamo-02.png",
+    imageSrc: "/svg/icono-telefono.svg",
     title: "El equipo de Maxilana se comunicará contigo",
     description: "Espera la llamada, a la brevedad un agente de Maxilana se comunicara contigo."
   },
   {
     id: 3,
-    imageSrc: "/pasos-prestamo-03.png",
+    imageSrc: "/svg/icono-pin-mapa.svg",
     title: "Acude a tu sucursal más cercana",
     description: "Visita una de nuestras sucursales, con todos los requisitos solicitados y en una plazo de 24 horas resolvemos."
   },
   {
     id: 4,
-    imageSrc: "/pasos-prestamo-04.png",
+    imageSrc: "/svg/icono-bolsa-dinero.svg",
     title: "Te entregamos tu dinero",
     description: "A partir de ese momento, puedes hacer pagos a capital y reducir tus pagos."
   },
@@ -188,25 +232,25 @@ const howItWorks = [
 const benefits = [
   {
     id: 1,
-    imageSrc: "/pasos-prestamo-01.png",
+    imageSrc: "/svg/icono-bolsa-dinero.svg",
     title: "Pago a capital",
     description: "Abona a capital cuando lo decidas, baja tu deuda y tus pagos parciales. Sin penalización."
   },
   {
     id: 2,
-    imageSrc: "/pasos-prestamo-02.png",
+    imageSrc: "/svg/icono-cronometro.svg",
     title: "Pago puntual",
     description: "Al pagar puntualmente tu crédito el último abono se te bonifica."
   },
   {
     id: 3,
-    imageSrc: "/pasos-prestamo-03.png",
+    imageSrc: "/svg/icono-persona.svg",
     title: "Tasa de interés personalizada",
     description: "Si tu primer crédito fue pagado puntual el segundo obtiene un beneficio de una tasa más baja."
   },
   {
     id: 4,
-    imageSrc: "/pasos-prestamo-04.png",
+    imageSrc: "/svg/icono-calculadora.svg",
     title: "Restructuración de tu crédito",
     description: "Tu crédito puede ser reestructurado a la mitad de tiempo estimado (se liquida el préstamo actual y se otorga la diferencia)"
   },
