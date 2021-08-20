c0de6b6414482fcc55fe0fc592387edaa732e85e
Efraín Rochín <erochin@grupoadhoc.mx>
2021-08-19 18:00:18 -0600

    Navbar con drawer

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -1,9 +1,9 @@
-
 import Link from 'next/link';
-import { FC, MouseEvent, ReactElement } from "react";
+import { FC, MouseEvent, ReactElement } from 'react';
 import { LoadingOutlined } from '@ant-design/icons';
 
 import styles from './Button.module.css';
+import cn from 'classnames';
 
 interface Props {
   text: string;
@@ -11,20 +11,21 @@ interface Props {
   loading?: boolean;
   icon?: ReactElement;
   rightIcon?: ReactElement;
-  variant?: "link" | "default";
-  size?: "small" | "default" | "large";
-  theme?: "default" | "primary" | "secondary" | "danger" | "whatsapp";
+  variant?: 'link' | 'default';
+  size?: 'small' | 'default' | 'large';
+  theme?: 'default' | 'primary' | 'secondary' | 'danger' | 'whatsapp';
+  className?: string;
   onClick?: (evt: MouseEvent<HTMLButtonElement>) => void;
-};
+}
 
 const classStyles = {
   size: {
-    default: "",
+    default: '',
     small: styles.sm,
     large: styles.lg,
   },
   theme: {
-    default: "",
+    default: '',
     primary: styles.primary,
     secondary: styles.secondary,
     danger: styles.danger,
@@ -39,14 +40,15 @@ const Button: FC<Props> = ({
   rightIcon,
   onClick,
   loading = false,
-  size = "default",
-  theme = "default",
-  variant = "default",
+  size = 'default',
+  theme = 'default',
+  variant = 'default',
+  className,
 }) => {
   const sizeStyles = classStyles.size[size];
   const themeStyles = classStyles.theme[theme];
-  const linkStyles = variant === "link" ? styles.link : "";
-  const className = [styles.root, sizeStyles, themeStyles, linkStyles].join(" ");
+  const linkStyles = variant === 'link' ? styles.link : '';
+  const rootClassName = cn(styles.root, sizeStyles, themeStyles, linkStyles, className);
 
   let iconElement = icon;
 
@@ -57,38 +59,21 @@ const Button: FC<Props> = ({
   if (href !== undefined) {
     return (
       <Link href={href}>
-        <a className={className}>
-          {icon && (
-            <span className={styles.iconContainer}>
-              {iconElement}
-            </span>
-          )}
+        <a className={rootClassName}>
+          {icon && <span className={styles.iconContainer}>{iconElement}</span>}
           <span>{text}</span>
         </a>
       </Link>
-    )
+    );
   }
 
-
   return (
-    <button
-      onClick={onClick}
-      disabled={loading}
-      className={className}
-    >
-      {(loading || icon) && (
-        <span className={styles.leftIconWrapper}>
-          {iconElement}
-        </span>
-      )}
+    <button onClick={onClick} disabled={loading} className={rootClassName}>
+      {(loading || icon) && <span className={styles.leftIconWrapper}>{iconElement}</span>}
       <span>{text}</span>
-      {rightIcon && (
-        <span className={styles.rightIconWrapper}>
-          {rightIcon}
-        </span>
-      )}
+      {rightIcon && <span className={styles.rightIconWrapper}>{rightIcon}</span>}
     </button>
-  )
-}
+  );
+};
 
 export default Button;
