6d30d8ab83d56edfabb35066aaa8ea2d855b0c49
Efraín Rochín <erochin@grupoadhoc.mx>
2021-09-15 18:21:30 -0600

    valida que el dominio de google maps y youtube se agregue a la variable de entorno de las imagenes

--- next.config.js
+++ next.config.js
@@ -8,9 +8,17 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const buildId = nextBuildId.sync({ dir: __dirname });
 
 if (!process.env.IMAGES_DOMAINS) throw Error('Environment variable IMAGES_DOMAINS is missing');
-if (!process.env.IMAGES_DEVICE_SIZES) throw Error('Environment variable IMAGES_DEVICE_SIZES is missing');
+if (!process.env.IMAGES_DEVICE_SIZES) {
+  throw Error('Environment variable IMAGES_DEVICE_SIZES is missing');
+}
 if (!process.env.IMAGES_SIZES) throw Error('Environment variable IMAGES_SIZES is missing');
 
+if (!`${process.env.IMAGES_DOMAINS}`.includes('maps.googleapis.com')) {
+  throw Error('Environment variable IMAGES_DOMAINS not includes maps.googleapis.com');
+}
+if (!`${process.env.IMAGES_DOMAINS}`.includes('img.youtube.com')) {
+  throw Error('Environment variable IMAGES_DOMAINS not includes img.youtube.com');
+}
 module.exports = withPreact(
   withBundleAnalyzer({
     target: 'serverless',
@@ -18,8 +26,8 @@ module.exports = withPreact(
     reactStrictMode: true,
     images: {
       domains: process.env.IMAGES_DOMAINS.split(','),
-      deviceSizes: process.env.IMAGES_DEVICE_SIZES.split(',').map(item => parseInt(item)),
-      imageSizes: process.env.IMAGES_SIZES.split(',').map(item => parseInt(item)),
+      deviceSizes: process.env.IMAGES_DEVICE_SIZES.split(',').map((item) => parseInt(item)),
+      imageSizes: process.env.IMAGES_SIZES.split(',').map((item) => parseInt(item)),
     },
   }),
 );
