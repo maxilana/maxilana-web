840c2d26cd877e0c20b6bca8b3c28cd62ff6132b
Efraín Rochín <erochin@grupoadhoc.mx>
2021-09-29 17:25:31 -0600

    pone imagenes en otros servicios

--- pages/otros-servicios.tsx
+++ pages/otros-servicios.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next';
+import Image from 'next/image';
 import getAllLegalPages from '~/api/cms/getAllLegalPages';
 import getOtherServices from '~/api/cms/getOtherServices';
 import getAllCities from '~/api/getAllCities';
@@ -26,11 +27,19 @@ const OtrosServicios: NextPage<Props> = ({ cities, legalPages, otherServices }) 
   return (
     <Layout title="Otros servicios" cities={cities} legalPages={legalPages}>
       <main className="container mx-auto p-4">
-        <div className="max-w-5xl space-y-24 pt-12 mx-auto">
+        <div className="max-w-5xl space-y-12 pt-12 pb-24 mx-auto">
           <h1 className="text-center h2">Otros Servicios</h1>
-          <div className="grid md:grid-cols-2 gap-24">
+          <div className="grid md:grid-cols-2 gap-8">
             {otherServices.map((item) => (
               <Card className="prose max-w-none" key={item.id}>
+                <div className="relative aspect-w-16 aspect-h-7 rounded overflow-hidden">
+                  <Image
+                    src={item?.image?.url as string}
+                    alt={item?.image?.alternativeText}
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
                 <h2>{item.title}</h2>
                 <CMSContent content={item.description} />
               </Card>
