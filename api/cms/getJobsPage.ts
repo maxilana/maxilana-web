bc77325efd0f5c5e8dfb5b9deca218b76a80bb2a
Ezequiel Rangel <chekelinho@gmail.com>
2021-09-29 14:27:04 -0600

    Conecta página de empleos al API

---
+++ api/cms/getJobsPage.ts
@@ -0,0 +1,30 @@
+import gql from 'graphql-tag';
+import { IMAGE_FIELDS_FRAGMENT, SEO_FIELDS_FRAGMENT } from './fragments';
+import graphqlFetcher from '../graphqlFetcher';
+import { CMSJobsPage } from '~/types/Models/CMSJobsPage';
+
+const getJobsPage = async (): Promise<CMSJobsPage> => {
+  const response = await graphqlFetcher<{ page: CMSJobsPage }>(gql`
+    ${IMAGE_FIELDS_FRAGMENT}
+    ${SEO_FIELDS_FRAGMENT}
+    query JobsPlatformPage {
+      page: vacancy {
+        seo {
+          ...SeoFields
+        }
+        jobsPlatform {
+          id
+          name
+          link
+          logo {
+            ...ImageFields
+          }
+        }
+      }
+    }
+  `);
+
+  return response?.page;
+};
+
+export default getJobsPage;
