5c832823dcd730931a3460b9ccbe8f9b7305be8a
Efraín Rochín <erochin@grupoadhoc.mx>
2021-10-08 11:51:38 -0600

    prueba con las fuentes en _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import type { AppProps } from 'next/app';
+import type { AppProps, NextWebVitalsMetric } from 'next/app';
+import Head from 'next/head';
 import Script from 'next/script';
 import NextNprogress from 'nextjs-progressbar';
 
@@ -9,6 +10,14 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <Script strategy="lazyOnload" src="https://embed.tawk.to/573b5cc8bf3b0fde536a3ddc/default" />
       <Component {...pageProps} />
       <NextNprogress
@@ -20,6 +29,10 @@ function MyApp({ Component, pageProps }: AppProps) {
       />
     </>
   );
+}
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  console.log(metric);
 }
 
 export default MyApp;
