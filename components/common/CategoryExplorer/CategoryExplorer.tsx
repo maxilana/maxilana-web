a21a17157b5715bc7839d1b4ce9079d87c1dacdc
Ezequiel Rangel <chekelinho@gmail.com>
2021-10-14 15:27:23 -0600

    Agrega el query search al category explorer

--- components/common/CategoryExplorer/CategoryExplorer.tsx
+++ components/common/CategoryExplorer/CategoryExplorer.tsx
@@ -13,26 +13,30 @@ const CategoryExplorer: FC<Props> = ({ categories }) => {
   return (
     <div className={styles.root}>
       <div className={styles.gridWrapper}>
-        {categories.map((item) => (
-          <div key={item.id} className={styles.gridElement}>
-            <Link href={`/busqueda?categoria=${item?.id}&orden=desc`}>
-              <a className={styles.link}>
-                <div className="w-[56px] h-[56px]">
-                  {!!item?.image?.url && (
-                    <Img
-                      src={item?.image?.url}
-                      width={item?.image?.width}
-                      height={item?.image?.height}
-                      layout="fixed"
-                      alt={item?.image?.alternativeText || item?.name}
-                    />
-                  )}
-                </div>
-                <span className={styles.linkLabel}>{item.name}</span>
-              </a>
-            </Link>
-          </div>
-        ))}
+        {categories.map((item) => {
+          const querySearch = item.filters?.search ? `&q=${item.filters?.search}` : '';
+
+          return (
+            <div key={item.id} className={styles.gridElement}>
+              <Link href={`/busqueda?categoria=${item?.id}&orden=desc${querySearch}`}>
+                <a className={styles.link}>
+                  <div className="w-[56px] h-[56px]">
+                    {!!item?.image?.url && (
+                      <Img
+                        src={item?.image?.url}
+                        width={item?.image?.width}
+                        height={item?.image?.height}
+                        layout="fixed"
+                        alt={item?.image?.alternativeText || item?.name}
+                      />
+                    )}
+                  </div>
+                  <span className={styles.linkLabel}>{item.name}</span>
+                </a>
+              </Link>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
