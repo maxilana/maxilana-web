fd8452baa9390e129780e11cd9404ef6f8be4db2
Ezequiel Rangel <chekelinho@gmail.com>
2021-10-21 15:59:25 -0600

    Maqueta de página de registro

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -5,32 +5,39 @@ import { Logo } from '~/components/svg';
 import { InputField } from '~/components/common';
 import { BareLayout } from '~/components/layout';
 import { Button } from '~/components/ui';
+import defaultValidateMessages from 'config/validationMessages';
 
 const LoginPage = () => {
   const [form] = Form.useForm();
 
+  const handleSubmit = (values: any) => {
+    console.log(values);
+  };
+
   return (
-    <BareLayout title="Iniciar Sesión" hasHeader={false}>
+    <BareLayout title="Iniciar Sesión" hasHeader={false} meta={{ css: ['/antd/form.css'] }}>
       <div className="flex flex-col items-center justify-center min-h-screen">
-        <header className="flex items-center justify-center my-8">
+        <header className="flex items-center justify-center my-4">
           <Link href="/">
             <a className="inline-flex">
               <Logo alt width={182} height={46} />
             </a>
           </Link>
         </header>
-        <div className="container mx-auto my-12 px-4 sm:max-w-lg">
+        <div className="container mx-auto my-4 px-4 sm:max-w-lg">
           <h1 className="h5 text-center my-8">Inicia sesión con tu email o celular</h1>
           <Form
             form={form}
             name="loginForm"
+            onFinish={handleSubmit}
+            validateMessages={defaultValidateMessages}
             className="bg-white rounded border border-gray-200 p-6"
           >
             <div className="grid gap-4">
-              <Form.Item name="user">
+              <Form.Item name="user" rules={[{ required: true }]}>
                 <InputField label="Email o celular" placeholder="Ingresa número celular o email" />
               </Form.Item>
-              <Form.Item name="password">
+              <Form.Item name="password" rules={[{ required: true }]}>
                 <InputField type="password" label="Contraseña" />
               </Form.Item>
               <Button fullWidth theme="primary" text="Iniciar Sesión" />
---
+++ pages/auth/registro.tsx
@@ -0,0 +1,117 @@
+import { Form } from 'antd';
+import Link from 'next/link';
+
+import { Logo } from '~/components/svg';
+import { InputField } from '~/components/common';
+import { BareLayout } from '~/components/layout';
+import { Button } from '~/components/ui';
+import defaultValidateMessages from 'config/validationMessages';
+
+const SignupPage = () => {
+  const [form] = Form.useForm();
+
+  const handleSubmit = (values: any) => {
+    console.log(values);
+  };
+
+  return (
+    <BareLayout title="Registro" hasHeader={false} meta={{ css: ['/antd/form.css'] }}>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <header className="flex items-center justify-center my-4">
+          <Link href="/">
+            <a className="inline-flex">
+              <Logo alt width={182} height={46} />
+            </a>
+          </Link>
+        </header>
+        <div className="container mx-auto my-4 px-4 sm:max-w-2xl">
+          <h1 className="h5 text-center my-8">
+            Regístrate y empieza a obtener beneficios de Maxilana
+          </h1>
+          <Form
+            form={form}
+            name="signupForm"
+            onFinish={handleSubmit}
+            validateMessages={defaultValidateMessages}
+            className="bg-white rounded border border-gray-200 p-6"
+          >
+            <div className="grid gap-4 sm:grid-cols-2">
+              <Form.Item name="name" className="sm:col-span-2" rules={[{ required: true }]}>
+                <InputField label="Nombre" />
+              </Form.Item>
+              <Form.Item name="lastname" rules={[{ required: true }]}>
+                <InputField label="Apellido paterno" />
+              </Form.Item>
+              <Form.Item name="surname" rules={[{ required: true }]}>
+                <InputField label="Apellido materno" />
+              </Form.Item>
+              <Form.Item name="celphone" rules={[{ required: true }]}>
+                <InputField type="tel" label="Número celular" maxLength={10} />
+              </Form.Item>
+              <Form.Item name="email" rules={[{ required: true }]}>
+                <InputField type="email" label="Correo electrónico" />
+              </Form.Item>
+              <Form.Item name="password" rules={[{ required: true }]}>
+                <InputField type="password" label="Contraseña" />
+              </Form.Item>
+              <Form.Item
+                name="confirmPassword"
+                rules={[
+                  { required: true },
+                  ({ getFieldValue }) => ({
+                    validator(_, value) {
+                      if (!value || getFieldValue('password') === value) {
+                        return Promise.resolve();
+                      }
+
+                      return Promise.reject(new Error('Las contraseñas no coinciden'));
+                    },
+                  }),
+                ]}
+              >
+                <InputField type="password" label="Confirmar contraseña" />
+              </Form.Item>
+              <div className="sm:col-span-2">
+                <Button fullWidth theme="primary" text="Regístrate" />
+              </div>
+            </div>
+            <hr className="my-6" />
+            <div className="text-center text-sm">
+              <p>
+                Al registrarte aceptas el{' '}
+                <Link href="/legal/aviso-de-privacidad">
+                  <a className="text-link">Aviso de privacidad</a>
+                </Link>{' '}
+                y los{' '}
+                <Link href="/legal/terminos-y-condiciones">
+                  <a className="text-link">Términos y condiciones</a>
+                </Link>
+              </p>
+            </div>
+          </Form>
+        </div>
+        <footer className="my-12 mx-auto sm:max-w-xl">
+          <ul className="flex flex-col items-center justify-between text-sm sm:flex-row">
+            <li className="text-link uppercase p-1">
+              <Link href="/contacto">
+                <a>Contacto</a>
+              </Link>
+            </li>
+            <li className="text-link uppercase p-1">
+              <Link href="/legal/terminos-y-condiciones">
+                <a>Términos y Condiciones</a>
+              </Link>
+            </li>
+            <li className="text-link uppercase p-1">
+              <Link href="/legal/aviso-de-privacidad">
+                <a>Aviso de privacidad</a>
+              </Link>
+            </li>
+          </ul>
+        </footer>
+      </div>
+    </BareLayout>
+  );
+};
+
+export default SignupPage;
