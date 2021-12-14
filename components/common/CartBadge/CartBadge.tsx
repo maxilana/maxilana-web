1e268547b50ce861be8eff1f16f10cfaac53fc4f
Ezequiel Rangel <chekelinho@gmail.com>
2021-12-14 11:18:37 -0700

    Nuevo badge para carrito de compras

--- components/common/CartBadge/CartBadge.tsx
+++ components/common/CartBadge/CartBadge.tsx
@@ -3,17 +3,16 @@ import Link from 'next/link';
 import { ShoppingCartOutlined } from '@ant-design/icons';
 
 import useCart from '~/hooks/cart/useCart';
+import styles from './CartBadge.module.css';
 
 const CartBadge: FC = () => {
   const { data } = useCart();
 
   return (
     <Link href="/carrito" prefetch={false}>
-      <a>
-        <span className="text-white text-sm inline-flex items-baseline">
-          {data !== undefined && data.products.length}
-          <ShoppingCartOutlined style={{ fontSize: 18 }} />
-        </span>
+      <a className={styles.root}>
+        <small className={styles.badge}>{data !== undefined && data.products.length}</small>
+        <ShoppingCartOutlined style={{ fontSize: 24 }} />
       </a>
     </Link>
   );
