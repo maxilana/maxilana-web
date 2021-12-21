43da858d89902253cbedc929e147d01073351d68
Efraín Rochín <erochin@grupoadhoc.mx>
2021-12-21 11:22:15 -0700

    hace preload de la fuente y pone el fallback en noscript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import type { AppProps } from 'next/app';
+import type { AppProps, NextWebVitalsMetric } from 'next/app';
 import Head from 'next/head';
 import NextNprogress from 'nextjs-progressbar';
 import MarketingScripts from '~/components/common/MarketingScripts';
@@ -11,14 +11,15 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   usePageView();
-
+  const font = 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=fallback';
   return (
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="preload" as="style" href={font} />
         {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
+          href={font}
           rel="stylesheet"
           media="print"
           onLoad={function (event) {
@@ -26,6 +27,9 @@ function MyApp({ Component, pageProps }: AppProps) {
             this.media = 'all';
           }}
         />
+        <noscript>
+          <link href={font} rel="stylesheet" />
+        </noscript>
       </Head>
       {/* Google Tag Manager - Global base code */}
       <MarketingScripts />
@@ -41,8 +45,8 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-/*export function reportWebVitals(metric: NextWebVitalsMetric) {
-  console.log(metric);
-}*/
+export function reportWebVitals(metrics: NextWebVitalsMetric) {
+  reportWebVitals(metrics);
+}
 
 export default MyApp;
