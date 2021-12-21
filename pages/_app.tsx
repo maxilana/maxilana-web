f7ed9a5c8ea1802f631334fba4b2709d718cdd5b
Efraín Rochín <erochin@grupoadhoc.mx>
2021-12-21 12:01:14 -0700

    quita el console log de webVitals y el font-display

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   usePageView();
-  const font = 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=fallback';
+  const font = 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap';
   return (
     <>
       <Head>
