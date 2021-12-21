cbc8de0ee73d11ee816ff6986ce995893a3ca553
Efraín Rochín <erochin@grupoadhoc.mx>
2021-12-21 11:22:35 -0700

    reporte de webvitals

--- utils/gtm.ts
+++ utils/gtm.ts
@@ -1,3 +1,4 @@
+import { NextWebVitalsMetric } from 'next/app';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
@@ -24,4 +25,18 @@ export const usePageView = () => {
       router.events.off('routeChangeComplete', pageview);
     };
   }, [router.events]);
+};
+
+export const reportWebVitals = ({ id, name, label, value }: NextWebVitalsMetric) => {
+  // Use `window.gtag` if you initialized Google Analytics as this example:
+  // https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_document.js
+  const event = {
+    event: name,
+    event_category: label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+    value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    event_label: id, // id unique to current page load
+    non_interaction: true, // avoids affecting bounce rate.
+  };
+  console.log(window?.dataLayer?.push, event);
+  window?.dataLayer?.push?.(event);
 };
